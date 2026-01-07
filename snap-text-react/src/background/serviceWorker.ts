bbffced41fca2846fb4c1e9fd1712077141536dd
JAlavindu <lavi156perera@gmail.com>
2026-01-07 22:05:45 +0530

    updating serviceWorker.ts

--- snap-text-react/src/background/serviceWorker.ts
+++ snap-text-react/src/background/serviceWorker.ts
@@ -7,10 +7,25 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//     if(info.menuItemId === "snaptext_extract" && tab?.id) {
+//         chrome.tabs.sendMessage(tab.id, { action: "extract_text" });
+//         chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       func: (imageUrl: string) => {
+//         window.postMessage(
+//           { type: "START_OCR", imageUrl },
+//           "*"
+//         );
+//       },
+//       args: [info.srcUrl ?? ""]
+//     });
+//     }
+// });
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if(info.menuItemId === "snaptext_extract" && tab?.id) {
-        chrome.tabs.sendMessage(tab.id, { action: "extract_text" });
-        chrome.scripting.executeScript({
+  if (info.menuItemId === "snaptext_extract" && tab?.id) {
+    chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: (imageUrl: string) => {
         window.postMessage(
@@ -20,8 +35,9 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       },
       args: [info.srcUrl ?? ""]
     });
-    }
+  }
 });
+
 
 
 
